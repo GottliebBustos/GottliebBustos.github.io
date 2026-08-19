6a5fe9a60b3abf62149587f10a25beaa96359ea8
GottliebBustos <got3000@gmail.com>
2026-08-19 00:28:30 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+   site: 'https://github.io',
+  base: '/GottliebBustos.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
